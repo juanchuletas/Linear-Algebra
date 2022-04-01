18235939f28186a0fc8c9c98541f903aeee7fc62
juanchuletas <gamj@xanum.uam.mx>
2022-03-31 18:49:10 -0600

    FAS

--- LUDECOMP/main.cpp
+++ LUDECOMP/main.cpp
@@ -4,10 +4,10 @@
 int main(){
 
    double matA[25]={2.,1.,1.,3.,2.,
-                 1.,2.,2.,1.,1.,
-                 1.,2.,9.,1.,5.,
-                 3.,1.,1.,7.,1.,
-                 2.,1.,5.,1.,8.};
+                    1.,2.,2.,1.,1.,
+                    1.,2.,9.,1.,5.,
+                    3.,1.,1.,7.,1.,
+                    2.,1.,5.,1.,8.};
 
    double LU[25];
 
@@ -25,10 +25,10 @@ int main(){
     lucmp.solve(bvec,x);*/
     for(int i=0; i<rows; i++){
         for(int j=0; j<rows; j++){
-                printf("\t%lf ",matA[i*rows+ j]);
+                //printf("\t%.10lf ",matA[i*rows+ j]);
         }
         printf("\n");
-        printf("%lf\n",bvec[i]);
+        printf("%.10lf\n",bvec[i]);
     }
 
 
